a8f27eb32e059047cdcedc94f69d62abaa301ddb
Roxanne Agerone <argonkryptonneon@gmail.com>
2018-07-08 14:54:22 -0700

    added folder with first cpp program

---
+++ firstprogram/firstprogram/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  firstprogram
+//
+//  Created by Roxanne Agerone on 7/7/18.
+//  Copyright © 2018 Roxanne Agerone. All rights reserved.
+//
+
+#include <iostream>
+
+int main()
+{
+    std::cout << "Hello, World!" << std::endl;
+    return 0;
+}
